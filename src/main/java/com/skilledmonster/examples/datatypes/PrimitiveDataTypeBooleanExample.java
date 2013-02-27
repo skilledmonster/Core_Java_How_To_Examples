845262862e6ae96b091f95dd87fdbb00a97a7a0d
Jagadeesh <admin@skilledmonster.com>
2013-02-26 23:47:23 -0500

    primite data type examples are added

---
+++ src/main/java/com/skilledmonster/examples/datatypes/PrimitiveDataTypeBooleanExample.java
@@ -0,0 +1,30 @@
+package com.skilledmonster.examples.datatypes;
+
+/**
+ * Primitive Data Type - boolean Example
+ * 
+ * This example shows how to declare and use primitive boolean variable
+ * 
+ * @author Jagadeesh
+ * 
+ */
+public class PrimitiveDataTypeBooleanExample {
+
+	public static void main(String[] args) {
+		// declare a boolean variable and set value to true
+		boolean b1 = true;
+		// display
+		System.out.println("Value of boolean variable b1 is :" + b1);
+
+		// declare a boolean variable and set value to false
+		boolean b2 = false;
+		// display
+		System.out.println("Value of boolean variable b2 is :" + b2);
+
+		// declare a boolean and store the result of the operation 10 > 2
+		boolean b3 = (10 > 2);
+		// display
+		System.out.println("Value of boolean variable b3 is :" + b3);
+	}
+
+}
